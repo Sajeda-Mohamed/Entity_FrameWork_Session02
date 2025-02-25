376b7a9b628ca344de1b4bd45e0bfda8ade84a9c
Sajeda-Mohamed <sagedamohameed55@gmail.com>
2025-02-25 06:37:05 +0200

    Add project files.

---
+++ Entity_FrameWork_Session02/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Entity_FrameWork_Session02
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
